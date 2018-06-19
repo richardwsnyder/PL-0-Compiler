33a34968d4770a8f5d2a921081340fa438f2515e
richardwsnyder <33703059+richardwsnyder@users.noreply.github.com>
2018-06-18 21:36:59 -0400

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,7 @@
+In order to compile and run this compiler, run the following commands
+
+~$ gcc parser.c lexical.c vm.c
+~$ ./a.out -f filename.txt -l -a -v
+
+
+the "-f filename.txt" tells the program which file you are running. Do no omit this flag!
---
+++ vm.h
@@ -0,0 +1,104 @@
+//Austin Peace
+//Sebastian De La Cruz
+
+#include <stdio.h>
+#define MAX_STACK_HEIGHT 2000
+#define MAX_CODE_LENGTH 500
+#define MAX_LEXI_LEVELS 3
+#define NUM_OP_CODES 23
+#define MAX_OUTPUT_BUFFER_LENGTH 500
+#define MAX_REGISTERS 8
+
+int run_VM(char *filename, int input_flag);
+
+static char *opcodes[] = {
+	"ERROR",
+	"LIT",
+	"RTN",
+	"LOD",
+	"STO",
+	"CAL",
+	"INC",
+	"JMP",
+	"JPC",
+	"SIO",
+	"NEG",
+	"ADD",
+	"SUB",
+	"MUL",
+	"DIV",
+	"ODD",
+	"MOD",
+	"EQL",
+	"NEQ",
+	"LSS",
+	"LEQ",
+	"GTR",
+	"GEQ"
+};
+
+static char *opcodeslowercase[] = {
+	"error",
+	"lit",
+	"rtn",
+	"lod",
+	"sto",
+	"cal",
+	"inc",
+	"jmp",
+	"jpc",
+	"sio",
+	"neg",
+	"add",
+	"sub",
+	"mul",
+	"div",
+	"odd",
+	"mod",
+	"eql",
+	"neq",
+	"lss",
+	"leq",
+	"gtr",
+	"geq"
+};
+
+enum ISA {
+	zero,
+	LIT,
+	RTN,
+	LOD,
+	STO,
+	CAL,
+	INC,
+	JMP,
+	JPC,
+	SIO,
+	NEG,
+	ADD,
+	SUB,
+	MUL,
+	DIV,
+	ODD,
+	MOD,
+	EQL,
+	NEQ,
+	LSS,
+	LEQ,
+	GTR,
+	GEQ
+};
+
+typedef struct {
+	int op;
+	int r;
+	int l;
+	int m;
+}INSTRUCTION;
+
+typedef struct {
+	int sp;
+	int bp;
+	int pc;
+	INSTRUCTION ir;
+}CPU;
